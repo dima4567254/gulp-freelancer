48d99377b8260c10793aa1799a94617f67d3ec33
dima <dima.xipkov@gmail.com>
2022-01-13 21:38:34 +0200

    not Nosvg

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,12 @@
 import fileInclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
-        .pipe(fileInclude())
+
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
+
         .pipe(app.gulp.dest(app.path.build.html))
+        .pipe(webpHtmlNosvg())
+        .pipe(fileInclude())
 }
