57c74df0cf49eaf5b6ae7f93c501f7ce1d44ebed
dima <dima.xipkov@gmail.com>
2022-01-13 22:49:35 +0200

    not version-number

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,12 +1,29 @@
 import fileInclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
 
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
 
+
         .pipe(app.gulp.dest(app.path.build.html))
         .pipe(webpHtmlNosvg())
-        .pipe(fileInclude())
+        .pipe(fileInclude()).pipe(
+            versionNumber({
+                'value': '%DT%',
+                'append': {
+                    'key': '_v',
+                    'cover': 0,
+                    'to': [
+                        'css',
+                        'js',
+                    ]
+                },
+                'output': {
+                    'file': 'gulp/version.json'
+                }
+            })
+        )
 }
