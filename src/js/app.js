d750f25940d62aca73454f31866a58654f855a7c
dima <dima.xipkov@gmail.com>
2022-01-15 16:59:56 +0200

    пример swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
-import *  as fisFunctions from "./modules/functions.js";
+import * as fisFunctions from "./modules/functions.js";
 
 fisFunctions.isWebp(); /*flsFunctions*/
+
+import Swiper, {
+    Navigation,
+    Pagination
+} from 'swiper';
+
+const swiper = new Swiper(); /*тут можно через импорт подключать npm install swiper*/ /*для примера*/
